1f93761bbde2f8870241e0d1be87f47879a579d9
unknown <btkkentxx2002@gmail.com>
2022-08-09 22:50:25 +0700

    jv

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,13 @@
+package rikkei.academy;
+
+public class Main extends BinarySearchTest {
+    public static void main(String[] args){
+        System.out.println(binarySearch(list,2));        System.out.println(binarySearch(list,2));
+        System.out.println(binarySearch(list,11));
+        System.out.println(binarySearch(list,79));
+        System.out.println(binarySearch(list,1));
+        System.out.println(binarySearch(list,5));
+        System.out.println(binarySearch(list,80));
+
+    }
+}
